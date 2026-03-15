b0bcd9a0655a5c0d8099ff8be99bca278ad41ffe
Liam <liam3006x@gmail.com>
2026-03-15 13:50:20 +0000

    Refine auth layout

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -11,11 +11,13 @@ defineProps<{
     <div
         class="bg-muted flex flex-col  gap-6 items-center justify-center min-h-svh p-6 relative md:p-10"
     >
+        <!-- Upper BG Fill -->
         <div class="absolute inset-0 w-full">
-            <div class="h-[35vh] bg-primary" />
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="fill-current -mt-px text-primary">
+            <div class="h-[35vh] bg-primary/80" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="fill-current text-primary/80">
                 <path fill-opacity="1" d="M0,64L120,69.3C240,75,480,85,720,85.3C960,85,1200,75,1320,69.3L1440,64L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg>
         </div>
+
         <UPageCard :variant="variant ?? 'outline'" class="w-full max-w-md">
             <div class="flex flex-col gap-8">
                 <div class="flex flex-col items-center gap-4">
