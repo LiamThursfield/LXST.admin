1f4383ec665c80a89c19f38234a9c33292833847
Liam <liam3006x@gmail.com>
2026-03-17 21:08:01 +0000

    JS Layout cleanup

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
 import type { BreadcrumbItem } from '@/types';
+import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
 
 type Props = {
     breadcrumbs?: BreadcrumbItem[];
