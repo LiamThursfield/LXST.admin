f36e7dd415774b17d8e0f93e8329442c42200b62
Liam <liam3006x@gmail.com>
2026-03-23 22:43:36 +0000

    Add Central Settings/Dashboard controllers

---
+++ app/Http/Controllers/Central/Web/Settings/SecurityController.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Http\Controllers\Central\Web\Settings;
+
+use App\Http\Controllers\Global\Web\Settings\AbstractSecurityController;
+
+class SecurityController extends AbstractSecurityController {}
