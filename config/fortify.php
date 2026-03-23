d649974f1468e8a1bffeecf2d6879d68fe32959b
Liam <liam3006x@gmail.com>
2026-03-23 22:21:05 +0000

    Add fortify redirect for login

--- config/fortify.php
+++ config/fortify.php
@@ -77,6 +77,21 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Redirects
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the path where users will get redirected during
+    | authentication or password reset when the operations are successful
+    | and the user is authenticated. You are free to change this value.
+    |
+    */
+
+    'redirects' => [
+        'login' => '/dashboard',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
     | Fortify Routes Prefix / Subdomain
     |--------------------------------------------------------------------------
     |
